80ca3ee2cfa722ae1ca71c4af5fc81d8b8574dd4
fatma <itchirfatma@gmail.com>
2026-02-25 12:58:46 +0100

    Ajouter le frontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews(124)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online plateform that facilitate bying and selling of broducts or services over the internet serves as a virtual marketplace where businesses and individu showcase their products, interact with customers, and conduct transactions without the need for a physical presence. e-commerce websites have frained immence popularity due to their convinted accessibility, and the global reach they offer.</p>
+            <p>An e-commerce website is an online plateform that facilitate bying and selling of broducts or services over the internet serves as a virtual marketplace where businesses and individu showcase their products, interact with customers, and conduct transactions without the need for a physical presence. e-commerce websites have frained immence popularity due to their convinted accessibility, and the global reach they offer.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,59 @@
+import React, { useContext } from 'react'
+import './ProductDisplay.css'
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from '../../Context/ShopContext';
+
+const ProductDisplay = (props) => {
+
+    const {product} = props;
+    const {addToCart} = useContext(ShopContext);
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+            </div>
+            <div className="productdisply-img">
+                <img className='productdisplay-main-img' src={product.image} alt="" />
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-stars">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(124)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem quis repellendus, dolore iste animi rerum est quam ratione commodi quaerat alias quod eaque quos numquam ea necessitatibus eos distinctio consequatur.
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+                <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+                <p className="productdisplay-right-category"><span>Category :</span>Womes ,T-Short, crop Top </p> 
+                <p className="productdisplay-right-category"><span>Tags :</span>Womes ,Modern, Latest </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
